23971dded9159fd78563ecb43ad6616f294c73ff
Alex_Wachira_Kimotho <alex.wachira@student.moringaschool.com>
2022-11-14 09:13:45 +0300

    added some styling to the homepage

--- src/components/Page/Home.js
+++ src/components/Page/Home.js
@@ -1,21 +1,25 @@
-// import {React, useState }from 'react';
-// import Navbar from './Navbar';
-// import Footer from './Footer';
-// import Header from './Header';
+import {React, useState} from 'react';
+import Navbar from './Navbar';
+import Footer from './Footer';
+import Header from './Header';
 
+const body = {
+  maxWidth: '1200px',
+  padding: '0 1rem',
+  marginLeft: 'auto',
+  marginRight: 'auto',
+};
 
-// function Home() {
+function Home() {
+  const [isAdding, setIsAdding] = useState(false);
 
-//       const [isAdding, setIsAdding] = useState(false);
+  return (
+    <div style={body}>
+      <Navbar />
+      <Header setIsAdding={setIsAdding} />
+      <Footer />
+    </div>
+  );
+}
 
-
-//   return (
-//     <div>
-//       <Navbar />
-//       <Header setIsAdding={setIsAdding} />
-//       <Footer />
-//     </div>
-//   );
-// }
-
-// export default Home;
+export default Home;
