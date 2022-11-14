eb536f34d1a23254563259dcbaaca75e619f6ee6
Alex_Wachira_Kimotho <alex.wachira@student.moringaschool.com>
2022-11-14 07:48:19 +0300

    Added the Header.js component to the Teachers folder

--- src/components/Page/Teachers/Header.js
+++ src/components/Page/Teachers/Header.js
@@ -1,17 +1,17 @@
-// import React from "react";
+import React from "react";
 
-// function Header({ setIsAdding }) {
-//   return (
-//     <header>
-//       <h1>Class Management System</h1>
-//       <h2>Teachers</h2>
-//       <div style={{ marginTop: "30px", marginBottom: "18px" }}>
-//         <button onClick={() => setIsAdding(true)} className="round-button">
-//           Add Teachers
-//         </button>
-//       </div>
-//     </header>
-//   );
-// }
+function Header({ setIsAdding }) {
+  return (
+    <header>
+      <h1>Class Management System</h1>
+      <h2>Teachers</h2>
+      <div style={{ marginTop: "30px", marginBottom: "18px" }}>
+        <button onClick={() => setIsAdding(true)} className="round-button">
+          Add Teachers
+        </button>
+      </div>
+    </header>
+  );
+}
 
-// export default Header;
+export default Header;
